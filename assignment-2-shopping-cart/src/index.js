0391dd236ff485c0ef78c73677fc25b72be7df31
lowjustin <13962110+lowjustin@users.noreply.github.com>
2022-11-19 11:05:56 +0800

    Added tailwindcss

--- assignment-2-shopping-cart/src/index.js
+++ assignment-2-shopping-cart/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 // import Router from './components/Router';
 import App from './components/App';
-// import "./css/style.css";
+import "./css/style.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
---
+++ assignment-2-shopping-cart/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        'blue': '#b6d4f0',
+        'blue-light': '#dbeeff',
+        'blue-dark': '#6c88a3',
+        'brown': '#a3885b',
+        'brown-light': '#f0dab6',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+        'gray-dark': '#273444',
+      },
+    }
+  },
+  plugins: [],
+};
