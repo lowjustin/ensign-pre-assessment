5c933931beb70864ffbf026f1fc9c06280622f56
lowjustin <13962110+lowjustin@users.noreply.github.com>
2022-11-22 11:09:04 +0800

    Updated database connection and User model

---
+++ assignment-2-back-end/models/User.js
@@ -0,0 +1,20 @@
+const db = require("../db");
+const { DataTypes } = require("sequelize");
+
+const User = db.define(
+  "User",
+  {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      required: "Please enter a username",
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+  }
+);
+
+exports.User = User;
