05003058572309f6d7af78e2ef7e934804881a6b
lowjustin <13962110+lowjustin@users.noreply.github.com>
2022-11-22 14:44:46 +0800

    Updated Product model, User and auth routes and handlers

---
+++ assignment-2-back-end/models/Product.js
@@ -0,0 +1,35 @@
+const db = require("../db");
+const { DataTypes } = require("sequelize");
+
+const Product = db.define(
+  "Product",
+  {
+    title: {
+      type: DataTypes.STRING,
+      trim: true,
+      required: "Please enter a title",
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      trim: true,
+      required: "Please enter a price",
+    },
+    description: {
+      type: DataTypes.TEXT,
+      trim: true,
+      required: "Please enter a description",
+    },
+    category: {
+      type: DataTypes.STRING,
+      trim: true,
+      required: "Please enter a category",
+    },
+    image: {
+      type: DataTypes.STRING,
+      trim: true,
+      required: "Please enter a image",
+    },
+  }
+);
+
+exports.Product = Product;
