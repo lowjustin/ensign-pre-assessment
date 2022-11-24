d9912dccc2fc05a96dd565c533f0e5d56330a394
lowjustin <13962110+lowjustin@users.noreply.github.com>
2022-11-24 17:18:16 +0800

    Updated typefaces and styles

--- assignment-2-shopping-cart/tailwind.config.js
+++ assignment-2-shopping-cart/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -7,19 +9,21 @@ module.exports = {
     },
     extend: {
       colors: {
-        'red': '#f00',
-        'blue': '#b6d4f0',
-        'blue-light': '#dbeeff',
-        'blue-dark': '#6c88a3',
-        'brown': '#a3885b',
-        'brown-light': '#f0dab6',
-        'gray': '#666',
-        'gray-light': '#ccc',
-        'gray-dark': '#333',
+        red: "#f00",
+        blue: "#b6d4f0",
+        "blue-light": "#dbeeff",
+        "blue-dark": "#6c88a3",
+        brown: "#a3885b",
+        "brown-light": "#f0dab6",
+        gray: "#666",
+        "gray-light": "#ccc",
+        "gray-dark": "#333",
       },
-    }
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [require("@tailwindcss/forms")],
 };
