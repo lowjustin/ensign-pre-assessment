abf01ece99c6bfb80f7dad1ed1e846147bb3a218
lowjustin <13962110+lowjustin@users.noreply.github.com>
2022-11-24 16:00:39 +0800

    Updated Navigation styling, split navigation into different categories

--- assignment-2-shopping-cart/src/components/Navigation.js
+++ assignment-2-shopping-cart/src/components/Navigation.js
@@ -6,43 +6,62 @@ import { CartCount } from "./CartFunctions";
 export default function Navigation() {
   // shared state
   const [user, setUser] = useAtom(userAtom);
-  
+
   return (
-    <nav>
-      <ul className="flex gap-4">
-        <li>
-          <NavLink className="nav-item" to="/">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="nav-item" to="/logout">
-            Logout
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="nav-item" to="/products">
-            Products
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="nav-item" to="/orders">
-            Orders
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="nav-item" to="/cart">
-            Cart
-            {<CartCount /> ? (
-              <span className="p-1 rounded bg-brown text-white">
-                <CartCount />
-              </span>
-            ) : (
-              ""
-            )}
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="nav-home">
+        <ul className="flex gap-4">
+          <li>
+            <NavLink className="nav-item" to="/">
+              Home
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      {user ? (
+        <nav className="nav-main">
+          <ul className="flex gap-4">
+            <li>
+              <NavLink className="nav-item" to="/products">
+                Products
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-item" to="/orders">
+                Orders
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink className="nav-item" to="/cart">
+                Cart
+                {<CartCount /> ? (
+                  <span className="p-1 rounded bg-brown text-white">
+                    <CartCount />
+                  </span>
+                ) : (
+                  ""
+                )}
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        ""
+      )}
+      {user ? (
+        <nav className="nav-utils">
+          <ul className="flex gap-4">
+            <li>
+              <NavLink className="nav-item" to="/logout">
+                Logout
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        ""
+      )}
+    </div>
   );
 }
