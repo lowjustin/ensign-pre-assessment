c1828e5e52729c0b05beb42f57e6d9725da757e2
lowjustin <13962110+lowjustin@users.noreply.github.com>
2022-11-25 18:28:41 +0800

    Added "logged in as" feature to header

--- assignment-2-shopping-cart/src/components/Header.js
+++ assignment-2-shopping-cart/src/components/Header.js
@@ -1,13 +1,25 @@
-import { NavLink } from "react-router-dom";
+import { useAtom } from "jotai";
+import { userAtom } from "../lib/atoms";
 import Navigation from "./Navigation";
 
-export default function Header(props) {
+export default function Header() {
+  // shared state
+  const [user, setUser] = useAtom(userAtom);
+
   return (
     <header className="flex justify-end p-8">
       {/* <h1>
         <NavLink to="/">Shopping Cart Demo</NavLink>
       </h1> */}
-      <Navigation />
+
+      <div>
+        {user ? (
+          <div className="mb-4 text-right text-sm text-gray">
+            Logged in as <span className="font-bold">{user.username}</span>
+          </div>
+        ) : null}
+        <Navigation />
+      </div>
     </header>
   );
 }
