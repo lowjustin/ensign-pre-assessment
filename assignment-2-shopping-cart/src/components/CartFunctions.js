0edfd4578b922a6b617c38763a08e4ef2f037d1b
lowjustin <13962110+lowjustin@users.noreply.github.com>
2022-11-26 08:59:30 +0800

    Update CartFunctions.js

--- assignment-2-shopping-cart/src/components/CartFunctions.js
+++ assignment-2-shopping-cart/src/components/CartFunctions.js
@@ -62,13 +62,3 @@ export function CartCount() {
 
   return cartCount();
 }
-
-export function calcTotal(cart, products) {
-  let total = 0.0;
-  total = Object.keys(cart).reduce((prevTotal, key) => {
-    const product = products.find((p) => p.id === parseInt(key));
-    const count = cart[key];
-    return prevTotal + count * product.price;
-  }, 0);
-  return total;
-}
