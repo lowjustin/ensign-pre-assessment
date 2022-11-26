ee1db6ad58be045d15ab80ba806e1109d1aeab91
lowjustin <13962110+lowjustin@users.noreply.github.com>
2022-11-26 21:25:28 +0800

    Updated ProtectedRoute component to verify token

--- assignment-2-shopping-cart/src/components/ProtectedRoute.js
+++ assignment-2-shopping-cart/src/components/ProtectedRoute.js
@@ -1,8 +1,59 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { useLayoutEffect, useState } from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import axios from "axios";
+import { useAtom } from "jotai";
+import { userAtom } from "../lib/atoms";
+import LoadingSpinner from "./LoadingSpinner";
 
-export default function ProtectedRoute({ user, redirectPath = "/" }) {
-  if (!user) {
-    return <Navigate to={redirectPath} replace />;
+export default function ProtectedRoute({ redirectPath = "/" }) {
+  // shared state
+  const [user, setUser] = useAtom(userAtom);
+  
+  // internal state
+  const location = useLocation();
+  const [auth, setAuth] = useState(false);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useLayoutEffect(() => {
+    const verifyToken = async () => {
+      setLoading(true);
+      try {
+        const config = {
+          headers: {
+            Authorization: `token ${user.token}`,
+          },
+        };
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/login/verify`,
+          config
+        );
+        setAuth(true);
+        setError("");
+      } catch (error) {
+        setAuth(false);
+        setError(error);
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (user) verifyToken();
+  }, [location.pathname]);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  if (!auth) {
+    setUser(false);
+  
+    const invalidTokenState = {
+      type: "info",
+      message: "Invalid access token"
+    }
+
+    return <Navigate to={redirectPath} state={invalidTokenState} replace />;
   }
 
   return <Outlet />;
