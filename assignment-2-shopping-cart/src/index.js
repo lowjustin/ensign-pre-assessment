48aabc27277600f67079ffae1fa8ac0786ad166a
lowjustin <13962110+lowjustin@users.noreply.github.com>
2022-11-26 19:43:27 +0800

    Updated styling

--- assignment-2-shopping-cart/src/index.js
+++ assignment-2-shopping-cart/src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-// import Router from './components/Router';
 import App from "./components/App";
-import "./css/style.css";
+import "./style.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
