2ef6b794115c7068939b43520ad0f0fdef541b38
lowjustin <13962110+lowjustin@users.noreply.github.com>
2022-12-08 09:51:55 +0800

    Added CORS middleware

--- assignment-2-back-end/src/app.js
+++ assignment-2-back-end/src/app.js
@@ -1,6 +1,7 @@
 var dotenv = require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
+var cors = require("cors");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -16,6 +17,11 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
+
+app.use(cors({
+  origin: process.env.FRONT_END_URL || 'http://localhost:3000'
+}));
+app.options('*', cors());
 
 app.use(logger("dev"));
 app.use(express.json());
